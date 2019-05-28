a51eec29fdb8f34b4426b6daebb13e3cf67f0057
ranjtih <48471002+ranjtih@users.noreply.github.com>
2019-05-28 16:43:03 +0530

    Delete uppertolowerviceversa.py

--- uppertolowerviceversa.py
+++
@@ -1,7 +0,0 @@
-name=input("enter the name\n")
-len=len(name)
-for i in range(0,len):
-	if(ord(name[i])>=97 and ord(name[i])<=125):
-		print(chr(ord(name[i])-32),end="")
-	if(ord(name[i])>=65 and ord(name[i])<=90):
-		print(chr(ord(name[i])+32),end="")
